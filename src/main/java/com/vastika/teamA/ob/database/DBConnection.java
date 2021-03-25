cc9e4d3e5812d55014b413b865f94130b8a35624
ashishPractice <shresthaashish1@gmail.com>
2021-03-24 23:36:21 -0500

    Connection to db

---
+++ src/main/java/com/vastika/teamA/ob/database/DBConnection.java
@@ -0,0 +1,20 @@
+package com.vastika.teamA.ob.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    // Method that return the connection
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+
+        // loading the driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        // creating a connection
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/bank_db", "root", "@Shrestha");
+
+    }
+
+}
